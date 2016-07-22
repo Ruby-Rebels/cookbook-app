4f6d1d2e8e7ca47047f4c844ba0c4cfc1ff02806
Trevor Jones <trevordavidjones@gmail.com>
2016-07-22 09:39:07 -0400

    added a Recipe model and migration

---
+++ db/migrate/20160722133819_create_recipes.rb
@@ -0,0 +1,13 @@
+class CreateRecipes < ActiveRecord::Migration
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.string :chef
+      t.string :ingredients
+      t.string :directions
+      t.integer :prep_time
+
+      t.timestamps null: false
+    end
+  end
+end
