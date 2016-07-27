4e83648d45501ebb767d2d49ab5506440bbf7af7
Trevor Jones <trevordavidjones@gmail.com>
2016-07-27 09:44:53 -0400

    added model methods for ingredients and directions

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,22 @@
 class Recipe < ActiveRecord::Base
+  def ingredients_list
+    return ingredients.split(",")
+  end
+
+  def directions_list
+    return directions.split(",")
+  end
+
+  def friendly_created_at
+    created_at.strftime("%m/%d/%Y")
+  end
+
+  def friendly_prep_time
+    hours = prep_time / 60
+    minutes = prep_time % 60
+    result = ""
+    result += "#{hours} hours " if hours > 0
+    result += "#{minutes} minutes" if minutes > 0
+    result
+  end
 end
